e90271c90c5eb3bc126dc3d1e542fb9a666d9cee
sadeeshan910 <sadeeshanperera910@gmail.com>
2025-03-13 00:25:35 +0530

    chore: modify eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [
       js.configs.recommended,
